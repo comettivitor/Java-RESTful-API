4eb940f3ee32701a63455cfca07f1a315e4cd67d
unknown <comettivitor@gmail.com>
2023-10-17 14:40:38 -0300

    Commit

---
+++ src/main/java/Santanderbackend2023/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package Santanderbackend2023.domain.repository;
+
+import Santanderbackend2023.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}
---
+++ src/main/java/Santanderbackend2023/service/UserService.java
@@ -0,0 +1,7 @@
+package Santanderbackend2023.service;
+
+import Santanderbackend2023.domain.model.User;
+
+public interface UserService extends CrudService<Long, User> {
+
+}
